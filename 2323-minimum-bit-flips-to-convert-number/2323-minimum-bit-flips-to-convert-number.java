25ed7b842983eb48d973c4a2aae64f697513ce3d
Amarthya Sabu <amarthya.sabu2006@gmail.com>
2026-09-18 15:02:47 +0530

    2323-MINIMUM-BIT-FLIPS-TO-CONVERT-NUMBER Stats: Time: 0 ms (100%), Space: 42.4 MB (19.16%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -1,12 +1,18 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
+        // first approach
         int dist = 0;
-        while(start!=0 || goal!=0){
-            int k1 = start%2;
-            int k2 = goal%2;
-            if(k1!=k2) dist++;
-            start /= 2;
-            goal /= 2;
+        // while(start!=0 || goal!=0){
+        //     int k1 = start%2;
+        //     int k2 = goal%2;
+        //     if(k1!=k2) dist++;
+        //     start /= 2;
+        //     goal /= 2;
+        // }
+        int xor = start^goal;
+        while(xor!=0){
+            dist += xor&1;
+            xor >>= 1;
         }
         return dist;
     }
